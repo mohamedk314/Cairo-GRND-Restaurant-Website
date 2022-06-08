516b6a6e29516e63c7e97275b783981bf162b8d4
mohamedk314 <98399157+mohamedk314@users.noreply.github.com>
2022-06-08 10:23:01 -1000

    Update receipt.php

--- receipt.php
+++ receipt.php
@@ -183,5 +183,6 @@
 						
 				</table>
 				<button style="margin-top:5px;" type="button" class="btn btn-success" onclick="window.location.href='ordertest.php'" >place order</button>
+				<button style="margin-top:5px;" type="button" class="btn btn-danger" onclick="window.location.href='cancelorder.php'" >cancel order</button>
 			</div>
 </div>
