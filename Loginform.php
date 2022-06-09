9d06cc5677aadb8f91954f87c346905a1ac3b954
Hazem7777 <86380432+Hazem7777@users.noreply.github.com>
2022-06-09 19:00:42 +0200

    Update Loginform.php

--- Loginform.php
+++ Loginform.php
@@ -1,7 +1,22 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
+<meta name="viewport" content=
+            "width=device-width, initial-scale=1">
+    <link rel="stylesheet" href=
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+     
+    <script src=
+"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+    </script>
+     
+    <script src=
+"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+    </script>
+     
+    <script src=
+"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+    </script>
 <html >
 
 <style>
@@ -14,14 +29,16 @@ body{
  justify-content: center;
  align-items: center;
 }
+
 .Container{
  padding: 10;
  width: 30%;
- height: 35%;
+ height: 45%;
  background-color: white;
  border-radius: 20px;
  box-shadow: 0px 25px 40px #1687d933;
 }
+
 .Header{
  margin: auto;
  width: 90%;
@@ -30,20 +47,27 @@ body{
  justify-content: space-between;
  align-items: center;
 }
+
 .Heading{
  font-size: 20px;
- font-family: ‘Open Sans’;
+ font-family: 'Open Sans';
  font-weight: 700;
  color: #2F3841;
 }
+
 input[type=text], [type=password] {
-  
-  width: 80%;
+  font-size: medium;
+  width: 90%;
   padding: 12px 20px;
   border-radius: 20px;
   margin: 8px 10;
   box-sizing: border-box;
 }
+
+label{
+  font-size: 14px;
+}
+
 button[type=button], button[type=submit]
 {
   background-color: rgba(0,0,0,0.7);
@@ -51,16 +75,18 @@ button[type=button], button[type=submit]
   color: white;
   padding: 16px 32px;
   text-decoration: none;
-  margin: 4px 2px;
+  margin: -10px 5px;
   cursor: pointer;
  border-radius: 20px;
  cursor: pointer;
  font-size: 16px;
  font-weight: 400;
 }
+
 .button:hover {
   color: skyblue;
 }
+
 .centered {
   position: fixed;
   top: 62%;
@@ -68,17 +94,33 @@ button[type=button], button[type=submit]
   margin-top: -50px;
   margin-left: -100px;
 }
+
+input::placeholder{
+font-size: medium;
+}
+
 .centered1 {
   position: fixed;
-  top: 70%;
+  top: 72%;
   left: 52%;
   margin-top: -50px;
   margin-left: -100px;
+  font-size: medium;
 }
+
+/* .checkbox2 {
+  position: fixed;
+  top: 55%;
+  left:55%;
+  margin-top: -25px;
+  margin-left: -180px;
+} */
+
 </style>
 
 
 <body>
+
 <form class= Container action="Login.php" method="post">
 <div class=”Header”>
  <h3 class=”Heading”>Login</h3>
@@ -90,18 +132,62 @@ button[type=button], button[type=submit]
   <label for="password"><b>Password</b></label>
   <input type="password" placeholder="Enter Password" name="password" required>
   <br>  <br>
+  
   <div class= "checkbox">
   <label >
     <input  type="checkbox" checked="checked" name="remember"> Remember me
   </label>
   </div>
   <br><br>
-  <div class=centered>
-  <button class="button" type="submit">Login</button>
+  
+  <div class="form-check form-check-inline">
+  <button class="button" type="submit" id="checked">Login</button>
   <button type="button" class="button">Cancel</button>
   <br>  <br>
   <span class=centered1 >Forgot <a href="#">password?</a></span>
+
+  <div class= "form-check form-check-inline">
+  <input class="form-check-input" type="radio" id="user" name="type" value="user" required>
+  <label for="user">User</label>
+  <input class="form-check-input" type="radio" id="cashier" name="type" value="cashier" required>
+  <label for="cashier">Cashier</label>
+  <input class="form-check-input" type="radio" id="control" name="type" value="control" required>
+  <label for="control">Control</label>
+ 
   </div>
+
+  <br><br>
+  </div>
+  
 </form>
-</body>
+
+<span id="msg"></span>
+    </div>
+     
+    <script>
+     
+        // On clicking submit do following
+        $("input[type=submit]").click(function() {
+             
+            var atLeastOneChecked = false;
+            $("input[type=radio]").each(function() {
+             
+                // If radio button not checked
+                // display alert message
+                if ($(this).attr("checked") != "checked") {
+                 
+                    // Alert message by displaying
+                    // error message
+                    $("#msg").html(
+        "<span class='alert alert-danger' id='error'>"
+        + "Please Choose atleast one</span>");
+                }
+            }
+			);
+        }
+		);
+    </script>
+	
+</body> 
+
 </html>
