c85fb2888ea9cc1cf899fe8c7e15799af909b242
YamiSakr <86329880+YamiSakr@users.noreply.github.com>
2022-06-10 20:34:12 +0200

    Update activate.php

--- activate.php
+++ activate.php
@@ -1,25 +1,25 @@
 <?php
   
-    // Connect to database 
-    $con=mysqli_connect("localhost","root","","grndrestraunt");
+          // Connect to database 
+      $con=mysqli_connect("localhost","root","","grndrestraunt");
   
     // Check if id is set or not if true toggle,
     // else simply go back to the page
-    if (isset($_GET['id'])){
+            if (isset($_GET['id'])){
   
-        // Store the value from get to a 
-        // local variable "course_id"
-        $course_id=$_GET['id'];
+                  // Store the value from get to a 
+                // local variable "course_id"
+            $course_id=$_GET['id'];
   
         // SQL query that sets the status
-        // to 1 to indicate activation.
-        $sql="UPDATE `userState` SET 
-             `status`=1 WHERE id='$course_id'";
-  
-        // Execute the query
-        mysqli_query($con,$sql);
-    }
-  
-    // Go back to course-page.php
-    header('location: course-page.php');
-?>
+            //to 1 to indicate activation.
+          $sql="UPDATE `userState` SET 
+               `status`=1 WHERE id='$course_id'";
+      
+          // Execute the query
+          mysqli_query($con,$sql);
+          }
+          
+          // Go back to course-page.php
+        header('location: course-page.php');
+      ?>
