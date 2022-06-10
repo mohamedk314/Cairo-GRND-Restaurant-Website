d37acce7e7194b05b6083134340f7ad7aad489a8
mohamedk314 <98399157+mohamedk314@users.noreply.github.com>
2022-06-10 10:31:28 -1000

    Update placingorder.php

--- placingorder.php
+++ placingorder.php
@@ -1,8 +1,11 @@
+
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 <style>
+
 button[type=button], button[type=submit]
 {
   background-color: rgba(0,0,0,0.7);
@@ -42,6 +45,7 @@ button[type=button], button[type=submit]
 
 
 <center>
+
 <h1>
 Time left in wich you can modify the order
 <span id="m"></span>
@@ -49,40 +53,54 @@ Time left in wich you can modify the order
 <span id="s"></span>
 </h1>
 <br>
+
 </center>
+
 <script>
+
 var s=59;
 var m=4;
 
 var time= setInterval(function() {timer()}, 1000);
 function timer(){
+
 s--;
 if(s==-1){
+
 m--;
 s=59;
 if(m==-1){
+
 m=0;
 s=0;
 clearInterval(time);
 location.href = 'ordertest.php';
+
 }
+
 }
+
 document.getElementById("m").innerHTML = m;
 document.getElementById("s").innerHTML = s;
+
 }
 
 </script>
 
 <head>
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 	    </head>
+
         <?php
         session_start();
         ?>
 
 <?php /*--------------------------------------------------          container                  --------------------------------------------------------------*/ ?>
+
 <div class="container">
       <div style="clear:both"></div>
 			<br />
@@ -96,12 +114,17 @@ document.getElementById("s").innerHTML = s;
 						<th width="15%">Total</th>
 						<th width="5%">Action</th>
 					</tr>
+
 <?php /*--------------------------------------------------          drinks                  --------------------------------------------------------------*/ ?>
+
 					<?php
+
                     $total_drinks = 0;
+
 					if(!empty($_SESSION["drinks_cart"]))
 					{
                         ?>
+                        
                         <tr>
 						<th width="70%" style = "color : red  ;  font-size: 20px;;">Drinks order</th>
 					</tr>
@@ -122,13 +145,20 @@ document.getElementById("s").innerHTML = s;
 						}
                         
 					}
+
                     $_SESSION['total_price_drinks'] = $total_drinks;
+
 					?>
+
 <?php /*--------------------------------------------------          dinner                  --------------------------------------------------------------*/ ?>
+
 					<?php
+
                     $total_dinner = 0;
+
 					if(!empty($_SESSION["dinner_cart"]))
 					{
+
                         ?>
                         <tr>
 						<th width="70%" style = "color : red  ;  font-size: 20px;;">dinner order</th>
@@ -150,10 +180,15 @@ document.getElementById("s").innerHTML = s;
 						}
                         
 					}
+
                     $_SESSION['total_price_dinner'] = $total_dinner;
+
 					?>
+
 <?php /*--------------------------------------------------          breakfast                  --------------------------------------------------------------*/ ?>
+
 					<?php
+
                     $total_breakfast = 0;
 					if(!empty($_SESSION["breakfast_cart"]))
 					{
@@ -178,14 +213,21 @@ document.getElementById("s").innerHTML = s;
 						}
                         
 					}
+
                     $_SESSION['total_price_breakfast'] = $total_breakfast;
+
                     ?>
+
 <?php /*--------------------------------------------------          lunch                  --------------------------------------------------------------*/ ?>
 
 <?php 
+
                     $total_lunch = 0;
+
 					if(!empty($_SESSION["lunch_cart"]))
+
 					{
+
                         ?>
                         <tr>
 						<th width="70%" style = "color : red  ;  font-size: 20px;;">lunch order</th>
@@ -196,25 +238,33 @@ document.getElementById("s").innerHTML = s;
 						{
 					?>
 					<tr>
+
 						<td><?php echo $values["item_name"]; ?></td>
 						<td><?php echo $values["item_quantity"]; ?></td>
 						<td>$ <?php echo $values["item_price"]; ?></td>
 						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
+
 					</tr>
+
 					<?php
+
 							$total_lunch = $total_lunch + ($values["item_quantity"] * $values["item_price"]);
                             
 						}
                         
 					}
+
                     $_SESSION['total_price_lunch'] = $total_lunch;
+
 					?>
 <?php /*--------------------------------------------------          compose                  --------------------------------------------------------------*/ ?>
 					
 					<?php
+
                     $total_compose = 0;
                     
 					if(!empty($_SESSION["sandwitch"]))
+
 					{
                         ?>
                         <tr>
@@ -234,15 +284,20 @@ document.getElementById("s").innerHTML = s;
 					<?php
 							$total_compose = $total_compose + ($values["item_quantity"] * $values["item_price"]);
                             $_SESSION['total_price_compose'] = $total_compose;
+
 						}
+
 					?>
 
                     <?php
 							
 						}
+
                         $total_receipt = $total_compose + $total_drinks + $total_lunch + $total_breakfast + $total_dinner;
                             $_SESSION['total_price_receipt'] = $total_receipt;
+
 					?>
+
 <?php /*--------------------------------------------------          total                  --------------------------------------------------------------*/ ?>
 					
 					<tr>
@@ -250,12 +305,17 @@ document.getElementById("s").innerHTML = s;
 						<td align:right>$ <?php echo number_format($total_receipt, 2); ?></td>
 						<td></td>
 					</tr>
+
 <center>
+
 <div>
+
   <button class="button" type="submit" onclick="window.location.href='ordertest.php'">skip timer</button>
   <button class="button" type="submit" onclick="window.location.href='hpu.php'">change order</button>
   <button type="button" class="button" onclick="window.location.href='receipt.php'">Cancel order!</button>
   
   <h3>note: of you skipped the timer you can not take it back !</h3>
+
   </div>
+
   </center>
