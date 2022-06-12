3ef517032ccc5eaa9220069be3b7e1083b8f8a80
mohamedk314 <98399157+mohamedk314@users.noreply.github.com>
2022-06-11 18:26:31 -1000

    Update view.php

--- view.php
+++ view.php
@@ -1,3 +1,40 @@
+<!DOCTYPE HTML>
+
+
+<div>
+	
+<link  href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<!-- Latest compiled and minified CSS -->
+<link  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
+
+<ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link active" href="view.php">comments</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="Ratingview.php">rating</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="promote.php">promotion</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="changestate.php">activation</a>
+  </li>
+</ul>
+</div>
+
+
+
 <html>
 
 <link rel="stylesheet" type="text/css" href="style.css">
